b2ce5ba3c034b60c80905b790759083f3a11fc4e
ArturoMrtzRomero <100323475+ArturoMrtzRomero@users.noreply.github.com>
2022-12-14 00:11:26 -0600

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name'=>'Arturo Martinez Romero',
+            'email'=>'admin@dealpc.com',
+            'password'=>bcrypt('frijol15')
+            ]);
     }
 }
